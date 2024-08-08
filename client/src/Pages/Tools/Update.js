2829fa15c31fc18bbea4ab8760b5a8489086fdfd
Rahul Gupta <154514374+rahulguptahotmail@users.noreply.github.com>
2024-08-09 03:30:03 +0530

    Update.js

--- client/src/Pages/Tools/Update.js
+++ client/src/Pages/Tools/Update.js
@@ -22,7 +22,7 @@ const Update = () => {
   const getData = async () => {
     const res = await axios.get(`http://localhost:5000/item/get/${id}`);
     setItem(res.data);
-    setTitle(res.data.title)
+    setTitle(res[0].data.title)
     setTopic(res.data.topic)
     setDescription(res.data.description)
   };
