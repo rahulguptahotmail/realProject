bc0449e2f45755cabe8712f733f9fe3e21dadda2
Rahul Gupta DevOps <154514374+rahulguptahotmail@users.noreply.github.com>
2026-04-21 02:38:01 -0700

    Update MongoDB connection URI with new user

--- Server/models/DB.js
+++ Server/models/DB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const uri = "mongodb+srv://rahulguptahotmail:UP56ac6331@cluster0.cgrfan7.mongodb.net/realProject";
+const uri = "mongodb+srv://rahulguptahotmail_db_user:UP56ac6331@cluster0.o3chn5f.mongodb.net/realproject";
 mongoose.connect(uri).then((res)=>{
     console.log("connected")
 }).catch((err)=>{
